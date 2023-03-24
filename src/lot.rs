e9eaf77d6a66e19ccbe6b21485245aabdfb65f95
Mark DeSpain <mark.despain@gmail.com>
2023-03-24 12:37:16 +0900

    add Lot

---
+++ src/lot.rs
@@ -0,0 +1,23 @@
+// a Lot an amount of securities purchased as a particular time
+#[derive(Debug)]
+pub struct Lot {
+
+    // the account within the lot is held
+    pub account: String,
+
+    // the symbol of the security held
+    // TODO: create a type that has validation?
+    pub symbol: String,
+
+    // the date that the lot was purchased
+    // TOOD: replace with a better "data" data type
+   pub  date: String,
+
+    // the number of shares of the security in this lot
+    pub quantity: u32,
+
+    // the per-share cost purchase price of this lot
+    // TOOD: add validation (can't be negative, infinity, NaN, etc.)
+    // TODO: replace with "money" type?
+    pub cost_basis: f32
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod lot;
+
+use crate::lot::Lot;
+
 fn main() {
-    println!("Hello, world!");
+    let lot = Lot {
+        account : String::from("Joint Taxable"),
+        symbol : String::from("VOO"),
+        date : String::from("2023/03/23"),
+        quantity : 6,
+        cost_basis : 321.55
+    };
+    println!("Hello, lot! {:?}", &lot);
 }
