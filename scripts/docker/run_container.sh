35b4470be1b4ef697c6ff293ad5fd4bc8a307d91
Mark DeSpain <mark.despain@gmail.com>
2023-04-07 20:13:01 +0900

    fix typo

--- scripts/docker/run_container.sh
+++ scripts/docker/run_container.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 docker network create --driver bridge portools-net
-:
-docker run --rm --name portools-mongo --network portools-net -d              mongo
-docker run --rm --name portools       --network portools-net -d -p 8080:8080 portools
+docker run --rm --name portools-mongo --network portools-net -d -p 27017:27017  mongo
+docker run --rm --name portools       --network portools-net -d -p 8080:8080    portools
