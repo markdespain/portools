80d16d2a1a08e47c9b69b9483db9f352ea79b824
Mark DeSpain <mark.despain@gmail.com>
2023-04-20 09:59:18 +0900

    run MongoDB as a single node replica set

--- scripts/docker/run_mongo_container.sh
+++ scripts/docker/run_mongo_container.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 ./scripts/docker/create_network_bridge.sh
-docker run --rm --name portools-mongo --hostname portools-mongo --network portools-net -d -p 27017:27017 mongo
+
+# Approach: Run MongoDB as a single node replica set.  A replicaa set is needed since the
+# portools-stream crate uses MongoDB Change Sreams.
+# ref: https://www.mongodb.com/docs/manual/changeStreams/
+
+docker run --rm --name portools-mongo --hostname portools-mongo --network portools-net -d -p 27017:27017 mongo \
+       --replSet "portools" --bind_ip_all
+
+echo "sleeping before initializing the replica set, so that MongoDB can first finishing initializing"
+sleep 5
+
+echo "Initializing the replica set"
+docker exec portools-mongo mongosh --eval "rs.initiate()"
+
+echo "replica set details:"
+docker exec portools-mongo mongosh --eval "rs.status()"
